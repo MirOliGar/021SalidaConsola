b8a2c2ac2d303865abb21de9fddedf13940bc6e4
Mireia Oliver <mireia.oliver.alum@iescamp.es>
2020-10-08 11:36:04 +0200

    Create 08-HoraEntrada.java

---
+++ 08-HoraEntrada.java
@@ -0,0 +1,27 @@
+/* 
+ * Copyright 2020 Mireia Oliver García.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+/**
+ *
+ * @author Mireia Oliver García - mireia.oliver.alum@iescamp.es
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.printf("Hora de entrada ....: %02d:%02dh\n", 12, 10);
+    }
+}
